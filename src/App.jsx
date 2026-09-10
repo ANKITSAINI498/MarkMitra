b8e6b493f3463d38f9b46694aa085d4086aeb251
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:06:34 +0530

    Auto-commit: src/App.jsx at 10/09/2026, 14:06:34

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import {Routes,Route,Navigate} from 'react-router-dom';
-import Workspace from './pages/Workspace';
-import Templates from './pages/Templates';
-import NotFound from './pages/NotFound';
-export default function App(){return <Routes><Route path="/" element={<Workspace/>}/><Route path="/templates" element={<Templates/>}/><Route path="*" element={<NotFound/>}/></Routes>}
+import { Routes, Route, Navigate } from "react-router-dom";
+import Workspace from "./pages/Workspace";
+import Templates from "./pages/Templates";
+import NotFound from "./pages/NotFound";
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Workspace />} />
+      <Route path="/templates" element={<Templates />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
