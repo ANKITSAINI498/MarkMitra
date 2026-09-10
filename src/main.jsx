6d725e42267695d0876e04afe3144dbd2f7f4ca2
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:02:17 +0530

    Auto-commit: src/main.jsx at 10/09/2026, 14:02:17

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
-import './index.css';
-import App from './App';
-import {MarkdownProvider} from './context/MarkdownContext';
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><MarkdownProvider><App/></MarkdownProvider></BrowserRouter></React.StrictMode>);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { MarkdownProvider } from "./context/MarkdownContext";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <MarkdownProvider>
+        <App />
+      </MarkdownProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
