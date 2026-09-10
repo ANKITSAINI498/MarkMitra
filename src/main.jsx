c2203e7ef06b53615577116176ed0bd17e6772fa
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:11:29 +0530

    Auto-commit: src/main.jsx at 10/09/2026, 14:11:29

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 import { MarkdownProvider } from "./context/MarkdownContext";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/MarkMitra/"}>
+   <BrowserRouter basename={import.meta.env.BASE_URL}>
       <MarkdownProvider>
         <App />
       </MarkdownProvider>
